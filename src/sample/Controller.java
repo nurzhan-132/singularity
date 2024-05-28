5948af905084f630b3374ca866e540e47d36d84f
Nurzhan Momynkul <nurzhanmomynov01@gmail.com>
2024-05-28 07:07:24 +0500

    hw-13 is done

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,106 @@
-package src.sample;
+package sample;
 
-public class Controller {
-}
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private TextArea questionArea;
+
+    @FXML
+    private RadioButton answer1, answer2, answer3, answer4;
+
+    @FXML
+    private ToggleGroup answers;
+
+    private String[] questions = {
+            "Ваш любимый фрукт?",
+            "Ваш любимый футболист?",
+            "Кто открыл Америку?",
+            "USSR collapsed in"
+    };
+
+    private String[][] variants = {
+            {"Банан", "Киви", "Апельсин", "Мандарин"},
+            {"Messi", "Penaldo", "Maradona", "Cruyff"},
+            {"Веспугчи", "Колумб", "Я", "Он"},
+            {"1945", "1917", "1991", "1979"},
+    };
+
+    private String[] correctVariants = {
+            "Апельсин",
+            "Penaldo",
+            "Колумб",
+            "1991"
+    };
+
+    private String[] userSelections = new String[questions.length];
+    private int currIndex = 0;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        loadQuestion(currIndex);
+    }
+
+    private void loadQuestion(int index) {
+        questionArea.setText(questions[index]);
+        answer1.setText(variants[index][0]);
+        answer2.setText(variants[index][1]);
+        answer3.setText(variants[index][2]);
+        answer4.setText(variants[index][3]);
+        answers.selectToggle(null);
+
+        // keep user selection
+        if (userSelections[index] != null) {
+            for (Toggle toggle : answers.getToggles()) {
+                RadioButton radioButton = (RadioButton) toggle;
+                if (radioButton.getText().equals(userSelections[index])) {
+                    answers.selectToggle(radioButton);
+                    break;
+                }
+            }
+        }
+    }
+
+    private void saveCurrentSelection() {
+        RadioButton selectedRadioButton = (RadioButton) answers.getSelectedToggle();
+        if (selectedRadioButton != null) {
+            userSelections[currIndex] = selectedRadioButton.getText();
+        }
+    }
+
+    public void tryToAnswer() {
+        saveCurrentSelection();
+        if (userSelections[currIndex] != null && userSelections[currIndex].equals(correctVariants[currIndex])) {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Правильно!");
+            alert.showAndWait();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Не правильно!");
+            alert.showAndWait();
+        }
+    }
+
+    @FXML
+    public void goToNext() {
+        saveCurrentSelection();
+        if (currIndex < questions.length - 1) {
+            currIndex++;
+            loadQuestion(currIndex);
+        }
+    }
+
+    @FXML
+    public void goToPrev() {
+        saveCurrentSelection();
+        if (currIndex > 0) {
+            currIndex--;
+            loadQuestion(currIndex);
+        }
+    }
+}   
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,4 +1,4 @@
-package src.sample;
+package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -9,10 +9,10 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("T-System");
+        primaryStage.setScene(new Scene(root, 600, 275));
         primaryStage.show();
     }
 
